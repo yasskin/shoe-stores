c9bc617c44405ac92c2d5e9f769c5422775707b2
Noah <yasskin@icloud.com>
2016-07-22 10:04:18 -0700

    add validation for presence of a tag category name and for max length of 20 characters

--- spec/tag_spec.rb
+++ spec/tag_spec.rb
@@ -1,1 +1,12 @@
 require('spec_helper')
+
+describe(Tag) do
+  it("validates that the form is not blank when a user enters a tag category name") do
+    new_category = Tag.new({:category => ""})
+    expect(new_category.save()).to(eq(false))
+  end
+  it("ensures the length of the category name for a new tag is at most 20 characters") do
+    long_category = Tag.new({:category => "n".*(21)})
+    expect(long_category.save()).to(eq(false))
+  end
+end
