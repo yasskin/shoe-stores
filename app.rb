ee2ebe69f695641d4202b8e8df67d649eb5f4e4c
Noah <yasskin@icloud.com>
2016-07-22 10:54:08 -0700

    add Store route with functionality to Create a new shoe store and take the user to that store page

--- app.rb
+++ app.rb
@@ -1,8 +1,28 @@
 require("bundler/setup")
 Bundler.require(:default)
 
-Dir[File.dirname(__FILE__) + 'lib/*rb'].each {|file| require file}
+Dir[File.dirname(__FILE__) + '/lib/*rb'].each {|file| require file}
 
 get('/') do
   erb(:index)
+end
+
+get('/stores') do
+  @stores = Store.all()
+  erb(:stores)
+end
+
+post('/stores') do
+  name = params.fetch('name')
+  @store = Store.new(:id => nil, :store_name => name)
+  if @store.save()
+    redirect('/stores/'.concat(@store.id().to_s()))
+  else
+    erb(:index)
+  end
+end
+
+get('/stores/:id') do
+  @store = Store.find(params.fetch('id').to_i())
+  erb(:store)
 end
--- spec/store_pages_spec.rb
+++ spec/store_pages_spec.rb
@@ -3,7 +3,7 @@ require('spec_helper')
 describe('create a route for the store model', {:type => :feature}) do
   it('creates a new shoe store and brings the user to a unique page for that store id') do
     visit('/')
-    click_button('Add Store')
+    click_button('Add New Shoe Store')
     expect(page).to have_content('NEW STORE')
   end
 end
