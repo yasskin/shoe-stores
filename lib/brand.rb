4635acacf1b2529ac00a4ad7cf54c48131e9aaa0
Noah <yasskin@icloud.com>
2016-07-22 09:58:45 -0700

    add validation that the user does not enter a blank field for a Brand and that the name is not longer than 30 characters

--- lib/brand.rb
+++ lib/brand.rb
@@ -1,5 +1,5 @@
 class Brand < ActiveRecord::Base
   has_and_belongs_to_many(:recipes)
-
+  validates(:brand_name, {:presence => true, :length => {:maximum => 30}})
 
 end
--- spec/brand_spec.rb
+++ spec/brand_spec.rb
@@ -1,1 +1,12 @@
 require('spec_helper')
+
+describe(Brand) do
+  it("validates that the form is not blank when a user enters a brand name") do
+    shoe_brand = Brand.new({:brand_name => ""})
+    expect(shoe_brand.save()).to(eq(false))
+  end
+  it("ensures the length of a name for a brand is at most 30 characters") do
+    shoe_brand = Brand.new({:brand_name => "n".*(31)})
+    expect(shoe_brand.save()).to(eq(false))
+  end
+end
