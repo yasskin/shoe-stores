a7e4904546652a45450762e2212783136d73744f
Noah <yasskin@icloud.com>
2016-07-25 21:08:14 -0700

    add functionality in app.rb and store page to view and add brands

--- app.rb
+++ app.rb
@@ -23,7 +23,7 @@ post('/stores') do
   if @store.save()
     redirect('/stores/'.concat(@store.id().to_s()))
   else
-    erb(:index)
+    erb(:store_errors)
   end
 end
 
@@ -33,34 +33,61 @@ post('/brands') do
   if @brand.save()
     redirect('/brands/'.concat(@brand.id().to_s()))
   else
-    erb(:index)
+    erb(:brand_errors)
   end
 end
 
 get('/stores/:id') do
   @store = Store.find(params.fetch('id').to_i())
-  @brands = @store.brands()
+  @store_brands = @store.brands()
+  @brands = Brand.all().-(@store_brands)
   erb(:store)
 end
 
 get('/brands/:id') do
   @brand = Brand.find(params.fetch('id').to_i())
-  @stores = @brand.stores()
+  @brand_stores = @brand.stores()
+  @stores = Store.all().-(@brand.stores)
   erb(:brand)
 end
 
-patch('/stores/:id') do
-  store_id = params.fetch('id').to_i()
-  @store = Store.find(store_id)
-  name = params.fetch('store-name')
-  if @store.update(:store_name => name)
+patch('/stores/:id/update') do
+  if params.fetch("form_id").==("update_store_name")
+    store_id = params.fetch('id').to_i()
+    @store = Store.find(store_id)
+    name = params.fetch('new_store_name')
+    if @store.update(:store_name => name)
+      redirect('/stores/'.concat(@store.id().to_s()))
+      erb(:store)
+    else
+      erb(:store_errors)
+    end
+  elsif params.fetch("form_id").==("update_shoe_brands")
+    store_id = params.fetch('id').to_i()
+    @store = Store.find(store_id)
+    new_brand_ids = params[:brand_ids]
+    brand_ids_array = []
+    @store.brands().each() do |brand|
+      brand_ids_array.push(brand.id())
+    end
+    new_brand_ids.each() do |id|
+      brand_ids_array.push(id)
+    end
+    @store.update({:brand_ids => brand_ids_array})
     redirect('/stores/'.concat(@store.id().to_s()))
-  else
-    erb(:store_errors)
+    erb(:store)
+  elsif params.fetch("form_id").==("add_shoe_brand")
+    store_id = params.fetch('id').to_i()
+    @store = Store.find(store_id)
+    name = params.fetch("shoe_brand_name")
+    @brand = @store.brands.new({:brand_name => name})
+    redirect('/stores/'.concat(@store.id().to_s()))
+    erb(:store)
   end
 end
 
-patch('/brands/:id') do
+patch('/brands/:id/update') do
+  if params.fetch("form_id").==("update_brand_name")
   brand_id = params.fetch('id').to_i()
   @brand = Brand.find(brand_id)
   name = params.fetch('brand-name')
